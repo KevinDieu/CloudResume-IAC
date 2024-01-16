0f2cbed145c0a4b0115e55bde8e1f9745a63dc74
KevinDieu <8922721+KevinDieu@users.noreply.github.com>
2024-01-16 11:18:43 -0600

    Update variable sensitivity and descriptions

--- variables.tf
+++ variables.tf
@@ -10,41 +10,49 @@ variable "org_id" {
 }
 
 variable "project_id" {
-  type = string
+  type        = string
+  description = "ID of target GCP Project"
 }
 
 variable "billing_account_id" {
   type        = string
+  sensitive   = true
   description = "ID of billing account to use for project"
 }
 
 variable "region" {
-  type    = string
-  default = "us-central1"
+  type        = string
+  default     = "us-central1"
+  description = "Target GCP Compute Region for resource deployment"
 }
 
 variable "zones" {
-  type    = list(string)
-  default = ["us-central1-c"]
+  type        = list(string)
+  default     = ["us-central1-c"]
+  description = "Target GCP CE Zones for resource deployment"
 }
 
 variable "size" {
-  type    = string
-  default = "e2-standard-2"
+  type        = string
+  default     = "e2-standard-2"
+  description = "Default Size for CE instances"
 }
 
 variable "cluster_count" {
-  type    = number
-  default = 1
+  type        = number
+  default     = 1
+  description = "Number of GKE Clusters to create"
 }
 
 variable "network_name" {
-  type = string
+  type        = string
+  description = "Name of managed VPC Network"
 }
 
 variable "subnetwork" {
-  type    = string
-  default = "subnet-01"
+  type        = string
+  default     = "subnet-01"
+  description = "Name of created VPC Subnet"
 }
 
 variable "labels" {
@@ -52,9 +60,11 @@ variable "labels" {
   default = {
     application = "cloudresume"
   }
+  description = "Standard set of labels to apply to resources"
 }
 
 variable "gke_service_account_name" {
-  type    = string
-  default = "gke-serviceaccount"
+  type        = string
+  default     = "gke-serviceaccount"
+  description = "Name of Service Account to use for GKE node pool operations"
 }
